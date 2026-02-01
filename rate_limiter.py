0086393ff831c364fcb813d70e85087172d9cb96
Ethan Kuo <ethankuocodes@gmail.com>
2026-02-01 18:53:18 -0500

    update rate limiter to only care about sec.gov

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,26 +1,43 @@
-from optparse import OptionParser
 from typing import Any
 from time import sleep, monotonic
 from arelle.utils.PluginHooks import PluginHooks
+from urllib.parse import urlparse
 import threading
+
+SEC_HOSTS = {"www.sec.gov", "data.sec.gov"}
 
 _lock = threading.Lock()
 _NEXT_ALLOWED = 0.0
+
 MIN_INTERVAL = .125 # 8 requests/sec
 _COUNT = 0
 
 def call_count():
-    return f"api calls: {_COUNT}"
-
-def wait():
-    global _NEXT_ALLOWED
-    global _COUNT
-    _COUNT += 1
     with _lock:
-        now = monotonic()
-        if now < _NEXT_ALLOWED:
-            sleep(_NEXT_ALLOWED - now)
-        _NEXT_ALLOWED = monotonic() + MIN_INTERVAL
+        return f"api calls: {_COUNT}"
+
+def _is_sec_http_url(url: str) -> bool:
+    if not url:
+        return False
+    if not (url.startswith("http://") or url.startswith("https://")):
+        return False
+    try:
+        host = urlparse(url).netloc.lower()
+    except Exception:
+        return False
+    return host in SEC_HOSTS
+
+def wait(url: str):
+    if _is_sec_http_url(url):
+        global _NEXT_ALLOWED
+        global _COUNT
+        with _lock:
+            _COUNT += 1
+            now = monotonic()
+            if now < _NEXT_ALLOWED:
+                sleep(_NEXT_ALLOWED - now)
+            _NEXT_ALLOWED = monotonic() + MIN_INTERVAL
+
     return True
 
 class RateLimiter(PluginHooks):
@@ -36,7 +53,7 @@ class RateLimiter(PluginHooks):
         if not url:
             return (None, False)
 
-        wait()
+        wait(url)
         
         return(url, False)
 
