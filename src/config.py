3d5077e7ae924090b4afc86b50e627b469505eef
Ethan Kuo <ethankuocodes@gmail.com>
2026-06-18 19:22:45 -0700

    update config.py structuring

--- src/config.py
+++ src/config.py
@@ -5,13 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DEFAULT_FILING_TYPES = ("10-K", "10-Q")
+
+ARELLE_PLUGINS_PATH = os.getenv("ARELLE_PLUGINS_PATH", "")
+
+SEC_COMPANY_TICKERS_URL = "https://www.sec.gov/files/company_tickers.json"
+
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-SEC_USER_AGENT = os.getenv("SEC_USER_AGENT")
-
 def db_kwargs() -> dict[str, Any]:
     """connection kwargs for psycopg.connect(**db_kwargs())."""
     return {
@@ -23,6 +27,7 @@ def db_kwargs() -> dict[str, Any]:
     }
 
 # https://www.sec.gov/about/webmaster-frequently-asked-questions#developers
+SEC_USER_AGENT = os.getenv("SEC_USER_AGENT")
 def sec_headers() -> dict[str, str]:
     """HTTP headers for requests to SEC EDGAR."""
     return {
@@ -30,7 +35,3 @@ def sec_headers() -> dict[str, str]:
         "Accept-Encoding": "gzip",
         "Accept": "application/json",
     }
-
-ARELLE_PLUGINS_PATH = os.getenv("ARELLE_PLUGINS_PATH", "")
-
-SEC_COMPANY_TICKERS_URL = "https://www.sec.gov/files/company_tickers.json"
